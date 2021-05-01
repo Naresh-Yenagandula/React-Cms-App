9ddcc49e22cd4dfdfbf07c96227797d3c1cfce3b
Naresh <nareshyenagandula98@gmail.com>
2021-05-01 15:05:00 +0530

    Page Curd operation

--- src/components/page.js
+++ src/components/page.js
@@ -1,26 +1,43 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/navbar';
-import { Container, Row, Col, ListGroup, Button, Breadcrumb,Table,Spinner } from 'react-bootstrap';
-import { FileEarmarkFill,PencilSquare,TrashFill } from 'react-bootstrap-icons';
-import {Link} from 'react-router-dom';
+import { Container, Row, Col, ListGroup, Button, Breadcrumb, Table, Spinner, Alert, Modal } from 'react-bootstrap';
+import { FileEarmarkFill, PencilSquare, TrashFill } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 function Page(props) {
-    const [pageData,setData]  =useState([])
-    const [message,setMessage] = useState()
-    const [isLoading,setLoading] = useState(true)
+    const [pageData, setData] = useState([])
+    const [message, setMessage] = useState()
+    const [isLoading, setLoading] = useState(true)
+    const [smShow, setSmShow] = useState({ view: false, id: '', message: '' });
 
     useEffect(() => {
         axios.get("http://localhost:8081/api/pages/0")
-        .then((result)=>{
-            setData(result.data.result)
-            setLoading(false)
-        })
-        .catch((err)=>{
-            setMessage({message:"Something went wrong",variant:"danger"})
-            setLoading(false)
-        })
+            .then((result) => {
+                setLoading(false)
+                if (result.data.result[0]) {
+                    setData(result.data.result)
+                } else {
+                    setMessage({ message: "No Data", variant: "" })
+                }
+            })
+            .catch((err) => {
+                setMessage({ message: "Something went wrong", variant: "danger" })
+                setLoading(false)
+            })
     }, [])
+
+    const deletePage = (id) => {
+        setSmShow({view:true,message:"Deleting..."})
+        axios.delete("http://localhost:8081/api/pages/" + id)
+            .then((result) => {
+                setData(pageData.filter(page => page._id !== id));
+                setSmShow({ view: true, message: "Page Deleted SuccessFully" })
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
 
     const dashboard = () => {
         props.history.push("/dashboard")
@@ -37,6 +54,29 @@ function Page(props) {
     return (
         <React.Fragment>
             <Navbar />
+            <Modal
+                size="sm"
+                show={smShow.view}
+                onHide={() => setSmShow({ view: false })}
+                animation={false}
+                centered
+                aria-labelledby="example-modal-sizes-title-sm"
+            >
+                {smShow.message ?
+                    <Modal.Header closeButton>
+                        {smShow.message}
+                    </Modal.Header> :
+                    <React.Fragment>
+                        <Modal.Header closeButton>
+                            Are you sure you want to delete this Page?
+                        </Modal.Header>
+                        <Modal.Footer>
+                            <Button variant="danger" size="sm" onClick={() => { deletePage(smShow.id) }}>Delete</Button>
+                            <Button variant="warning" size="sm" onClick={() => { setSmShow({ view: false, id: '', message: '' }) }}>Cancel</Button>
+                        </Modal.Footer>
+                    </React.Fragment>
+                }
+            </Modal>
             <Container className='mt-4'>
                 <Row>
                     <Col md={4}>
@@ -64,33 +104,36 @@ function Page(props) {
                                 Pages
                             </Breadcrumb.Item>
                         </Breadcrumb>
-                        <Table hover size="sm" className="text-center">
-                            <thead>
-                                <tr>
-                                    <th>Page title</th>
-                                    <th>Category</th>
-                                    <th>Author</th>
-                                    <th>Update</th>
-                                    <th>Delete</th>
-                                </tr>
-                            </thead>
-                            {isLoading?
-                            <Spinner animation="border" />:
-                            <tbody>
-                                {pageData.map((page)=>{
-                                    return(
-                                    <tr key={page._id}>
-                                        <td>{page.title}</td>
-                                        <td>{page.category}</td>
-                                        <td>{page.author}</td>
-                                        <td><Link to={`pages/update/${page._id}`}><Button size="sm"><PencilSquare></PencilSquare></Button></Link></td>
-                                        <td><Button size="sm"><TrashFill></TrashFill></Button></td>
+                        {isLoading ?
+                            <Spinner animation="border" /> :
+                            <Table hover size="sm" className="text-center">
+                                <thead>
+                                    <tr>
+                                        <th>Page title</th>
+                                        <th>Category</th>
+                                        <th>Author</th>
+                                        <th>Update</th>
+                                        <th>Delete</th>
                                     </tr>
-                                    )
-                                })}
-                            </tbody>
-}
-                        </Table>
+                                </thead>
+                                {message ?
+                                    <tbody><tr><td colSpan="5"><Alert variant={message.variant}>{message.message}</Alert></td></tr></tbody> :
+                                    <tbody>
+                                        {pageData.map((page) => {
+                                            return (
+                                                <tr key={page._id}>
+                                                    <td>{page.title}</td>
+                                                    <td>{page.category}</td>
+                                                    <td>{page.author}</td>
+                                                    <td><Link to={`pages/update/${page._id}`}><Button size="sm"><PencilSquare></PencilSquare></Button></Link></td>
+                                                    <td><Button onClick={() => setSmShow({ view: true, id: `${page._id}`, message: '' })} size="sm"><TrashFill></TrashFill></Button></td>
+                                                </tr>
+                                            )
+                                        })}
+                                    </tbody>
+                                }
+                            </Table>
+                        }
                     </Col>
                 </Row>
             </Container>
